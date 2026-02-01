e8dbf961b59e11ac95e60f955cce6973e2c6c076
madhukar4080 <madhukar4080@gmail.com>
2026-02-01 17:37:51 -0600

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+
+namespace MyDotNetApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            var jsonData = System.IO.File.ReadAllText("Data/sampledata.json");
+            var data = JsonSerializer.Deserialize<SampleData>(jsonData);
+            Message = data?.Message ?? "No message";
+        }
+    }
+
+    public class SampleData
+    {
+        public string Message { get; set; }
+        public string Date { get; set; }
+    }
+}
